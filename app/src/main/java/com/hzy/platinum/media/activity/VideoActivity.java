a5af7e1d068d96a284b6028efa22c25319dff564
caohuaihao <haochafeiming@gmail.com>
2021-06-11 11:46:24 +0800

    Mod: android P适配http

--- app/src/main/java/com/hzy/platinum/media/activity/VideoActivity.java
+++ app/src/main/java/com/hzy/platinum/media/activity/VideoActivity.java
@@ -2,9 +2,11 @@ package com.hzy.platinum.media.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.devbrackets.android.exomedia.listener.OnErrorListener;
 import com.devbrackets.android.exomedia.ui.widget.VideoControls;
 import com.devbrackets.android.exomedia.ui.widget.VideoControlsCore;
 import com.devbrackets.android.exomedia.ui.widget.VideoView;
@@ -37,6 +39,13 @@ public class VideoActivity extends BasePlayActivity {
         mVideoView.setOnPreparedListener(this);
         mVideoView.setOnCompletionListener(this);
         mVideoView.setOnBufferUpdateListener(this);
+        mVideoView.setOnErrorListener(new OnErrorListener() {
+            @Override
+            public boolean onError(Exception e) {
+                Log.e("_______video play error","______error:  "+e.getMessage());
+                return false;
+            }
+        });
     }
 
     @Override
